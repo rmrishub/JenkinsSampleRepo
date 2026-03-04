e6dbe98b78e3f5bd0a08c184f9e5761f660c1594
rmrishub <87750151+rmrishub@users.noreply.github.com>
2026-03-04 13:45:37 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class AdditionExample {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
+        System.out.println("Sum = " + sum);
+    }
+}
